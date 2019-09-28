a0a7c2ac39b696ae467dedb60484afccfa3e1dcb
joshuabp24 <54971712+joshuabp24@users.noreply.github.com>
2019-09-27 20:02:19 -0700

    Add files via upload

---
+++ Calculo/Figuras.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo
+{
+    public class Figuras
+    {
+        public string Nombre { get; set; }
+        public double LadoA { get; set; }
+        public double LadoB { get; set; }
+    }
+}
